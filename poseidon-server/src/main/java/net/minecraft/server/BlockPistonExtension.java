b74ffeddd296810288541dfa1eff93b91c7bc5c8
zavdav <157126752+zavdav@users.noreply.github.com>
2026-03-23 19:37:43 +0100

    Fix extended sticky pistons not dropping when broken at the piston head
    See https://github.com/retromcorg/Project-Poseidon/commit/5747a8a54d3377a37c80f73abd0f6095c9ac8d06

--- poseidon-server/src/main/java/net/minecraft/server/BlockPistonExtension.java
+++ poseidon-server/src/main/java/net/minecraft/server/BlockPistonExtension.java
@@ -16,7 +16,7 @@ public class BlockPistonExtension extends Block {
     public void remove(World world, int i, int j, int k) {
         super.remove(world, i, j, k);
         int l = world.getData(i, j, k);
-        if (l > 5 || l < 0) return; // CraftBukkit - fixed a piston AIOOBE issue.
+        if ((l & 7) >= PistonBlockTextures.a.length) return; // CraftBukkit - fixed a piston AIOOBE issue.
         int i1 = PistonBlockTextures.a[b(l)];
 
         i += PistonBlockTextures.b[i1];
@@ -139,7 +139,7 @@ public class BlockPistonExtension extends Block {
 
     public void doPhysics(World world, int i, int j, int k, int l) {
         int i1 = b(world.getData(i, j, k));
-        if (i1 > 5 || i1 < 0) return; // CraftBukkit - fixed a piston AIOOBE issue.
+        if ((i1 & 7) >= PistonBlockTextures.a.length) return; // CraftBukkit - fixed a piston AIOOBE issue.
         int j1 = world.getTypeId(i - PistonBlockTextures.b[i1], j - PistonBlockTextures.c[i1], k - PistonBlockTextures.d[i1]);
 
         if (j1 != Block.PISTON.id && j1 != Block.PISTON_STICKY.id) {
