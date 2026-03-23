3c66d066d3f34704a2c2f5df852e4c753b6021a5
zavdav <157126752+zavdav@users.noreply.github.com>
2026-03-23 15:28:43 +0100

    Fix multiplayer bow sounds
    See https://github.com/retromcorg/Project-Poseidon/commit/30304117b08b641045e14baad1ca632ed844bd6e https://github.com/retromcorg/Project-Poseidon/commit/d18a3e7b7d0dcae90593b7d9b01c99080afc9f73

--- poseidon-server/src/main/java/net/minecraft/server/ItemBow.java
+++ poseidon-server/src/main/java/net/minecraft/server/ItemBow.java
@@ -9,7 +9,8 @@ public class ItemBow extends Item {
 
     public ItemStack a(ItemStack itemstack, World world, EntityHuman entityhuman) {
         if (entityhuman.inventory.b(Item.ARROW.id)) {
-            world.makeSound(entityhuman, "random.bow", 1.0F, 1.0F / (b.nextFloat() * 0.4F + 0.8F));
+            // Poseidon - fix player bow sounds
+            world.a(entityhuman, 1002, MathHelper.floor(entityhuman.locX), MathHelper.floor(entityhuman.locY - (double) entityhuman.height), MathHelper.floor(entityhuman.locZ), 0);
             if (!world.isStatic) {
                 world.addEntity(new EntityArrow(world, entityhuman));
             }
