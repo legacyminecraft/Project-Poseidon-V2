081ca586cebb9c346359adf3e21704f05cb43b0c
zavdav <157126752+zavdav@users.noreply.github.com>
2026-03-23 19:20:52 +0100

    Fix blocks only harvestable with tool not dropping item on last tool use
    See https://github.com/retromcorg/Project-Poseidon/commit/a6f02789d826e9b77ae4dddf16b4535f26aba2b5

--- poseidon-server/src/main/java/net/minecraft/server/ItemInWorldManager.java
+++ poseidon-server/src/main/java/net/minecraft/server/ItemInWorldManager.java
@@ -168,17 +168,19 @@ public class ItemInWorldManager {
         boolean flag = this.b(i, j, k);
         ItemStack itemstack = this.player.G();
 
+        // Poseidon start - moved up to fix blocks only harvestable with tool not dropping item on last tool use
+        if (flag && this.player.b(Block.byId[l])) {
+            Block.byId[l].a(this.world, this.player, i, j, k, i1);
+            ((EntityPlayer) this.player).netServerHandler.sendPacket(new Packet53BlockChange(i, j, k, this.world));
+        }
+        // Poseidon end
+
         if (itemstack != null) {
             itemstack.a(l, i, j, k, this.player);
             if (itemstack.count == 0) {
                 itemstack.a(this.player);
                 this.player.H();
             }
-        }
-
-        if (flag && this.player.b(Block.byId[l])) {
-            Block.byId[l].a(this.world, this.player, i, j, k, i1);
-            ((EntityPlayer) this.player).netServerHandler.sendPacket(new Packet53BlockChange(i, j, k, this.world));
         }
 
         return flag;
